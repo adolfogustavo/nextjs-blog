a54d0716973ce8df4464e171a62e0f815b7150e6
adolfogustavo <gustavodominguez230@gmail.com>
2022-10-19 11:40:10 -0300

    chore: updating blog user' name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Gustavo. I'm software developer</p>
+        <p>Hello, I'm Gustavo Adolfo. I'm software developer</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
